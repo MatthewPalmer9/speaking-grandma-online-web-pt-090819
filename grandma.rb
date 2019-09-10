7f1ffd065860bf94af6bb31284247b7408b3167c
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-10 01:01:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if string.upcase
+  if string == string.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif string
     puts "NO, NOT SINCE 1938!"
