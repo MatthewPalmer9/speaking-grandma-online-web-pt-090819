b7b975f9f1ff4c4af29927901f3ea3ddc33f4e2e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-10 01:10:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(string)
     puts "NO, NOT SINCE 1938!"
   elsif string
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif string == love
+  elsif string = love
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
